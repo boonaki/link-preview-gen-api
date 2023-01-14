6e74b8869e3cad2b2394db1e35ddf7a5a22e9f44
boonaki <joshdurr17@gmail.com>
2023-01-13 21:56:05 -0500

    cors fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 const LinkPreviewGenerator = require('link-preview-generator')
 require('dotenv').config()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
