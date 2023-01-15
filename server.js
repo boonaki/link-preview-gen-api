78f9b6ef815f775ba9d61680a4a013ae5d293770
boonaki <joshdurr17@gmail.com>
2023-01-15 13:34:19 -0500

    solution 1

--- server.js
+++ server.js
@@ -7,13 +7,30 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/api/:link', async (req,res) => {
     const preview = await LinkPreviewGenerator(req.params.link)
-    res.json(preview)
+
+    // const response = {
+    //     statusCode: 200,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Credentials': true,
+    //     },
+    //     body: JSON.stringify({
+    //       preview: preview
+    //     }),
+    // };
+    res.json(response)
 })
 
 app.listen(process.env.PORT || 8000, (req,res) => {
