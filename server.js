4f1a24d2e9699378a73c8bfe6f9b6bb131319f90
boonaki <joshdurr17@gmail.com>
2023-01-15 14:26:03 -0500

    public use

--- server.js
+++ server.js
@@ -7,26 +7,19 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile('/index.html')
 })
 
 app.get('/api/:link', async (req,res) => {
     const preview = await LinkPreviewGenerator(req.params.link)
-
-    const response = {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': 'link-preview-gen-api.up.railway.app',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },
-        body: JSON.stringify({
-          preview: preview
-        }),
-    };
-    res.json(response)
+    res.json(preview)
 })
 
 app.listen(process.env.PORT || 8000, (req,res) => {
