83e3dbb58b6674969681c652c92cccb66d840885
boonaki <joshdurr17@gmail.com>
2023-01-19 17:11:47 -0500

    browser changes

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const LinkPreviewGenerator = require('link-preview-generator')
+const puppeteer = require('puppeteer')
 
 app.use(cors())
 app.use(express.static('public'))
+
+const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+});
 
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://link-preview-gen-api.up.railway.app");
