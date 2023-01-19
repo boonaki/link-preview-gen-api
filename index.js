e4b71872c1f3cdf11344653adbdb415ff0ee9790
boonaki <joshdurr17@gmail.com>
2023-01-19 17:35:55 -0500

    deployment fix

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+
 const LinkPreviewGenerator = require('link-preview-generator')
-const puppeteer = require('puppeteer')
+
 
 app.use(cors())
 app.use(express.static('public'))
 
-const browser = await puppeteer.launch({
-    'args' : [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-});
 
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://link-preview-gen-api.up.railway.app");
@@ -21,28 +15,13 @@ const browser = await puppeteer.launch({
 // });
 
 app.get('/', (req,res) => {
-    res.sendFile('/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/api/link', async (req,res) => {
-    try{
-        const preview = await LinkPreviewGenerator('https://boonaki.me')
-        console.log(preview)
-        // const response = {
-        //     statusCode: 200,
-        //     headers: {
-        //       'Access-Control-Allow-Origin': '*', // <-- Add your specific origin here
-        //       'Access-Control-Allow-Credentials': true,
-        //     },
-        //     body: JSON.stringify({
-        //       preview: preview
-        //     }),
-        // };
-        res.json(preview)
-    }catch(err){
-        console.log(err)
-        res.redirect('/')
-    }
+    const preview = await LinkPreviewGenerator('https://boonaki')
+    console.log(preview)
+    res.json(preview)
 })
 
 app.listen(process.env.PORT || 8000, (req,res) => {
